2243c1e01c799b1efbd7164ba4b750af476be9ee
alfedrouteca <diseno.uteca@gmail.com>
2024-02-14 19:51:07 -0600

    Tarea1
    Arreglar errores de codigo

---
+++ PPV2/Assets/Scripts/Depuracion.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Depuracion : MonoBehaviour
+{
+    public float velocidad = 5f;
+
+    void Update()
+    {
+        // Obtener la entrada del teclado
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        // Calcular el vector de movimiento
+        Vector3 movimiento = new Vector3 (movimientoHorizontal, 0f, movimientoVertical);
+
+        // Mover el objeto en la dirección calculada
+        transform.Translate (movimiento* velocidad * Time.deltaTime, Space.World);
+    }
+}
